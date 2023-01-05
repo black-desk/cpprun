4c3968bcf586d1343433829e0349b2f91e9055f8
black-desk <me@black-desk.cn>
2023-01-06 00:15:35 +0800

    test(cxx_oci_spec): print exception

--- test/oci_spec/config_test.cpp
+++ test/oci_spec/config_test.cpp
@@ -15,17 +15,22 @@
                 << "json parse failed";
 
 // NOLINTNEXTLINE
-#define VERIFY_PASS(...)                                                      \
-        cxx_oci_spec::Config config;                                          \
-        config_json.get_to(config);                                           \
-        std::shared_ptr<cxx_oci_spec::VerifyedConfig> verified_config;        \
-        ASSERT_NO_THROW(/*NOLINT*/                                            \
-                        verified_config =                                     \
-                                cxx_oci_spec::VerifyedConfig::verfiy(         \
-                                        data_path(), config,                  \
-                                        cxx_oci_spec::VerifyedConfig::Options \
-                                                __VA_ARGS__););               \
-        ASSERT_NE(verified_config, nullptr)                                   \
+#define VERIFY_PASS(...)                                                       \
+        cxx_oci_spec::Config config;                                           \
+        config_json.get_to(config);                                            \
+        std::shared_ptr<cxx_oci_spec::VerifyedConfig> verified_config;         \
+        ASSERT_NO_THROW(/*NOLINT*/                                             \
+                        try {                                                  \
+                                verified_config =                              \
+                                        cxx_oci_spec::VerifyedConfig::verfiy(  \
+                                                data_path(), config,           \
+                                                cxx_oci_spec::VerifyedConfig:: \
+                                                        Options __VA_ARGS__);  \
+                        } catch (const std::exception &e) {                    \
+                                std::cout << fmt::format("{}", e);             \
+                                throw;                                         \
+                        });                                                    \
+        ASSERT_NE(verified_config, nullptr)                                    \
                 << "verified_config should never be nullptr";
 
 // NOLINTNEXTLINE
